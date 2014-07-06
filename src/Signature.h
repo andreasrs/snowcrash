c6d8f072d39c3ddebc0caaaaba4ce34b938cb6e8
Z <z@apiary.io>
2014-07-06 11:58:05 +0200

    Add checking & warnings to the signature parser
    In addition the signature parser is now of standard-parser grade consuming
    node iterators and parser data. Also the signature parser now returns any
    remaining multiline content.

--- src/Signature.h
+++ src/Signature.h
@@ -9,9 +9,11 @@
 #ifndef SNOWCRASH_SIGNATURE_H
 #define SNOWCRASH_SIGNATURE_H
 
+#include "SourceAnnotation.h"
 #include "MarkdownNode.h"
 #include "RegexMatch.h"
 #include "StringUtility.h"
+#include "SectionParserData.h"
 
 namespace snowcrash {
     
@@ -23,30 +25,35 @@ namespace snowcrash {
     extern bool HasSectionKeywordSignature(const mdp::MarkdownNodeIterator& node);
     
     /**
-     *  \brief (List-defined) Signature Data
+     *  \brie   (List-defined) Signature Data
      *
      *  Data as parsed from a keyword-less list-defined signature.
      */
     struct Signature {
-        std::string identifier;             /// < Signature identifier
-        std::string value;                  /// < Signature value
-        std::vector<std::string> traits;    /// < Signature traits
-        std::string content;                /// < Signature content
+        std::string identifier;             /// Signature identifier
+        std::string value;                  /// Signature value
+        std::vector<std::string> traits;    /// Signature traits
+        std::string content;                /// Signature content
     };
     
     /**
-     *  Trait charactestics of a list-defined signature
+     *  \brief  Traits charactestics of a list-defined signature
+     *
+     *  Traits describe identifier - defined or value/type defined signatures.
+     *  If %expectIdentifier is true the signature must start with an identifier.
+     *  If %expectIdentifier is false and %allowValue or %maxTraitsCount is set
+     *  the signature must start with value, if any, or signature traits.
      */
     struct SignatureTraits {
         
-        const std::string contentDelimiter; /// < Signature content delimiter e.g. "-", "->", "..."
-        bool allowIdentifier;               /// < True to allow identifier in signature
-        bool allowValue;                    /// < True to allow value in signature
-        size_t maxTraitsCount;              /// < Max number of traits, 0 for no traits
-        bool allowContent;                  /// < True to allow content in signature
+        const std::string contentDelimiter; /// Signature content delimiter e.g. "-", "->", "..."
+        bool expectIdentifier;              /// True to expect an identifier in the signature
+        bool allowValue;                    /// True to allow value in signature
+        size_t maxTraitsCount;              /// Max number of traits, 0 for no traits
+        bool allowContent;                  /// True to allow content in signature
         
         SignatureTraits()
-        : contentDelimiter("-"), allowIdentifier(true),
+        : contentDelimiter("-"), expectIdentifier(true),
         allowValue(true), maxTraitsCount(2), allowContent(true) {}
         
         /** \return Default signature traits */
@@ -65,26 +72,54 @@ namespace snowcrash {
         static const char* const UnescapedValue;
         static const char* const CSVTraits;
         
-        static void parseSignature(const std::string& subject, const SignatureTraits& t, Signature& s) {
+        /**
+         *  \brief Parse list-defined line signature
+         *
+         *  \param  node    Node its text is to be parsed as the signature
+         *  \param  t       Signature traits
+         *  \param  pd      Parser data
+         *  \param  report  Output report buffer
+         *  \param  remainingContent    Remaining additional lines of node text buffer
+         *  \param  s       Parsed signature
+         */
+        static void parseSignature(const mdp::MarkdownNodeIterator& node,
+                                   const SignatureTraits& t,
+                                   const SectionParserData& pd,
+                                   Report& report,
+                                   mdp::ByteBuffer& remainingContent,
+                                   Signature& s) {
             
-            std::string work = subject;
+            mdp::ByteBuffer work = GetFirstLine(node->text, remainingContent);
             
-            if (t.allowIdentifier) {
+            if (t.expectIdentifier) {
                 s.identifier = RegexCaptureFirst(work, UnescapedIdentifier);
-                TrimString(s.identifier);
+
                 work = work.substr(s.identifier.size());
                 TrimStringStart(work);
+                
+                TrimString(s.identifier);
+                
+                if (s.identifier.empty()) {
+                    MissingIdentifierWarning(node, work, t, pd, report);
+                }
             }
             
             if (t.allowValue) {
-                if (!t.allowIdentifier || work.find_first_of(':') == 0) {
+                if (!t.expectIdentifier || work.find_first_of(':') == 0) {
                     
-                    if (t.allowIdentifier)
+                    if (t.expectIdentifier)
                         work = work.substr(1);
                     
                     s.value = RegexCaptureFirst(work, UnescapedValue);
+
                     work = work.substr(s.value.size());
+                    TrimStringStart(work);
+
                     TrimString(s.value);
+
+                    if (t.expectIdentifier && s.value.empty()) {
+                        MissingValueWarning(node, work, t, pd, report);
+                    }
                 }
             }
             
@@ -104,6 +139,10 @@ namespace snowcrash {
                     }
                 }
             }
+            
+            if (!t.expectIdentifier && s.value.empty() && s.traits.empty()) {
+                MissingElementDefinitionWarning(node, work, t, pd, report);
+            }
 
             if (t.allowContent) {
                 if (work.find(t.contentDelimiter) == 0) {
@@ -114,12 +153,100 @@ namespace snowcrash {
             }
             
             if (!work.empty()) {
-                // TODO: WARN
-                int i = 42;
-                ++i;
+                UnexpectedContentWarning(node, work, t, pd, report);
             }
         }
         
+        /** \brief Compose missing element definition warning */
+        static void MissingElementDefinitionWarning(const mdp::MarkdownNodeIterator& node,
+                                                    const mdp::ByteBuffer& work,
+                                                    const SignatureTraits& t,
+                                                    const SectionParserData& pd,
+                                                    Report& report) {
+            
+            // WARN: Missing element definition */
+            mdp::CharactersRangeSet sourceMap = mdp::BytesRangeSetToCharactersRangeSet(node->sourceMap, pd.sourceData);
+            report.warnings.push_back(Warning("missing element value or trait(s), 'expected '<value> (<traits>)'",
+                                              SignatureSyntaxWarning,
+                                              sourceMap));
+        }
+        
+        /** \brief Compose missing identifier warning */
+        static void MissingIdentifierWarning(const mdp::MarkdownNodeIterator& node,
+                                             const mdp::ByteBuffer& work,
+                                             const SignatureTraits& t,
+                                             const SectionParserData& pd,
+                                             Report& report) {
+            
+            // WARN: Missing identifier */
+            mdp::CharactersRangeSet sourceMap = mdp::BytesRangeSetToCharactersRangeSet(node->sourceMap, pd.sourceData);
+            report.warnings.push_back(Warning("missing expected identifier",
+                                              SignatureSyntaxWarning,
+                                              sourceMap));
+        }
+
+        /** \brief Compose missing value warning */
+        static void MissingValueWarning(const mdp::MarkdownNodeIterator& node,
+                                        const mdp::ByteBuffer& work,
+                                        const SignatureTraits& t,
+                                        const SectionParserData& pd,
+                                        Report& report) {
+            
+            // WARN: Missing value after ':` */
+            mdp::CharactersRangeSet sourceMap = mdp::BytesRangeSetToCharactersRangeSet(node->sourceMap, pd.sourceData);
+            report.warnings.push_back(Warning("missing value after ':', expected '<identifier>: <value>'",
+                                              SignatureSyntaxWarning,
+                                              sourceMap));
+        }
+        
+        
+        
+        /** \brief Compose unexpected content warning */
+        static void UnexpectedContentWarning(const mdp::MarkdownNodeIterator& node,
+                                             const mdp::ByteBuffer& work,
+                                             const SignatureTraits& t,
+                                             const SectionParserData& pd,
+                                             Report& report) {
+            
+            // WARN: Unexpected content
+            std::stringstream ss;
+            ss << "ignoring '" << work << "', expected '";
+            if (t.expectIdentifier) {
+                ss << "<identifier>";
+                
+                if (t.allowValue)
+                    ss << ":";
+            }
+            
+            if (t.allowValue) {
+                if (t.expectIdentifier)
+                    ss << " ";
+                
+                ss << "<value>";
+            }
+            
+            if (t.maxTraitsCount) {
+                ss << " (";
+                for (size_t i = 0; i < t.maxTraitsCount; ++i) {
+                    ss << "<t" << i + 1 << ">";
+                    if (i < t.maxTraitsCount - 1)
+                        ss << ", ";
+                }
+                
+                ss << ")";
+            }
+            
+            if (t.allowContent) {
+                ss << " " << t.contentDelimiter << " <content>";
+            }
+            
+            ss << "'";
+            
+            mdp::CharactersRangeSet sourceMap = mdp::BytesRangeSetToCharactersRangeSet(node->sourceMap, pd.sourceData);
+            report.warnings.push_back(Warning(ss.str(),
+                                              SignatureSyntaxWarning,
+                                              sourceMap));
+        }
     };
 }
 
--- test/test-Signature.cc
+++ test/test-Signature.cc
@@ -9,12 +9,36 @@
 #include "snowcrashtest.h"
 #include "Signature.h"
 
+static const mdp::ByteBuffer PropertySignatureFixture = "id: 42 (yes, no) - a good message";
+static const mdp::ByteBuffer ElementSignatureFixture = "42 (number) - a good number";
+
 using namespace snowcrash;
+
+struct SignatureParserHelper {
+    
+    static void parse(const mdp::ByteBuffer& signature,
+                      const SignatureTraits& traits,
+                      Report& report,
+                      mdp::ByteBuffer& remaining,
+                      Signature& s) {
+        
+        mdp::MarkdownNode n;
+        n.text = signature;
+        n.sourceMap.push_back(mdp::Range(0, n.text.size()));
+        mdp::MarkdownNodes v;
+        v.push_back(n);
+        
+        Blueprint bp;
+        SectionParserData pd(0, n.text, bp);
+        
+        SignatureParser::parseSignature(v.begin(), traits, pd, report, remaining, s);
+    }
+};
 
 struct ElementTraits : public SignatureTraits {
 
     ElementTraits() {
-        allowIdentifier = false;
+        expectIdentifier = false;
     }
     
     static ElementTraits& get() {
@@ -25,8 +49,16 @@ struct ElementTraits : public SignatureTraits {
 
 TEST_CASE("signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("id: 42 (yes, no) - a good message", SignatureTraits::get(), s);
+    
+    SignatureParserHelper::parse(PropertySignatureFixture,
+                                 SignatureTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier == "id");
     REQUIRE(s.value == "42");
@@ -36,10 +68,37 @@ TEST_CASE("signature", "[signature]")
     REQUIRE(s.content == "a good message");
 }
 
+TEST_CASE("mutliline signature", "[signature]")
+{
+    Report r;
+    mdp::ByteBuffer rc;
+    Signature s;
+    
+    SignatureParserHelper::parse("id\nLine 2\nLine 3\n",
+                                 SignatureTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc == "Line 2\nLine 3\n");
+    
+    REQUIRE(s.identifier == "id");
+    REQUIRE(s.value.empty());
+    REQUIRE(s.traits.empty());
+    REQUIRE(s.content.empty());
+}
+
 TEST_CASE("identifier only signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("id", SignatureTraits::get(), s);
+    
+    SignatureParserHelper::parse("id",
+                                 SignatureTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier == "id");
     REQUIRE(s.value.empty());
@@ -49,8 +108,16 @@ TEST_CASE("identifier only signature", "[signature]")
 
 TEST_CASE("identifier description signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("id - a good - info", SignatureTraits::get(), s);
+    
+    SignatureParserHelper::parse("id - a good - info",
+                                 SignatureTraits::get(), r, rc, s);
+
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier == "id");
     REQUIRE(s.value.empty());
@@ -60,8 +127,16 @@ TEST_CASE("identifier description signature", "[signature]")
 
 TEST_CASE("identifier value signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("id : a good data", SignatureTraits::get(), s);
+    
+    SignatureParserHelper::parse("id : a good data",
+                                 SignatureTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier == "id");
     REQUIRE(s.value == "a good data");
@@ -71,8 +146,16 @@ TEST_CASE("identifier value signature", "[signature]")
 
 TEST_CASE("identifier traits signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("id (number)", SignatureTraits::get(), s);
+    
+    SignatureParserHelper::parse("id (number)",
+                                 SignatureTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier == "id");
     REQUIRE(s.value.empty());
@@ -83,21 +166,36 @@ TEST_CASE("identifier traits signature", "[signature]")
 
 TEST_CASE("element signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("42 (yes, no) - a good message", ElementTraits::get(), s);
+    
+    SignatureParserHelper::parse(ElementSignatureFixture,
+                                 ElementTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier.empty());
     REQUIRE(s.value == "42");
-    REQUIRE(s.traits.size() == 2);
-    REQUIRE(s.traits[0] == "yes");
-    REQUIRE(s.traits[1] == "no");
-    REQUIRE(s.content == "a good message");
+    REQUIRE(s.traits.size() == 1);
+    REQUIRE(s.traits[0] == "number");
+    REQUIRE(s.content == "a good number");
 }
 
 TEST_CASE("element value only signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("42", ElementTraits::get(), s);
+    
+    SignatureParserHelper::parse("42",
+                                 ElementTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier.empty());
     REQUIRE(s.value == "42");
@@ -107,8 +205,16 @@ TEST_CASE("element value only signature", "[signature]")
 
 TEST_CASE("element traits only signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("(number)", ElementTraits::get(), s);
+    
+    SignatureParserHelper::parse("(number)",
+                                 ElementTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier.empty());
     REQUIRE(s.value.empty());
@@ -119,8 +225,16 @@ TEST_CASE("element traits only signature", "[signature]")
 
 TEST_CASE("element traits description signature", "[signature]")
 {
+    Report r;
+    mdp::ByteBuffer rc;
     Signature s;
-    SignatureParser::parseSignature("(number) - something () cool", ElementTraits::get(), s);
+    
+    SignatureParserHelper::parse("(number) - something () cool",
+                                 ElementTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.empty());
+    REQUIRE(rc.empty());
     
     REQUIRE(s.identifier.empty());
     REQUIRE(s.value.empty());
@@ -128,3 +242,82 @@ TEST_CASE("element traits description signature", "[signature]")
     REQUIRE(s.traits[0] == "number");
     REQUIRE(s.content == "something () cool");
 }
+
+TEST_CASE("identifier with element traits", "[signature]")
+{
+    Report r;
+    mdp::ByteBuffer rc;
+    Signature s;
+    
+    SignatureParserHelper::parse(PropertySignatureFixture,
+                                 ElementTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.size() == 1);
+    REQUIRE(rc.empty());
+    
+    REQUIRE(s.identifier.empty());
+    REQUIRE(s.value == "id");
+    REQUIRE(s.traits.empty());
+    REQUIRE(s.content.empty());
+}
+
+TEST_CASE("property signature without identifier", "[signature]")
+{
+    Report r;
+    mdp::ByteBuffer rc;
+    Signature s;
+    
+    SignatureParserHelper::parse("(x)",
+                                 SignatureTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.size() == 1);
+    REQUIRE(rc.empty());
+    
+    REQUIRE(s.identifier.empty());
+    REQUIRE(s.value.empty());
+    REQUIRE(s.traits.size() == 1);
+    REQUIRE(s.traits[0] == "x");
+    REQUIRE(s.content.empty());
+}
+
+TEST_CASE("property signature colon without a value", "[signature]")
+{
+    Report r;
+    mdp::ByteBuffer rc;
+    Signature s;
+    
+    SignatureParserHelper::parse("id: (number)",
+                                 SignatureTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.size() == 1);
+    REQUIRE(rc.empty());
+    
+    REQUIRE(s.identifier == "id");
+    REQUIRE(s.value.empty());
+    REQUIRE(s.traits.size() == 1);
+    REQUIRE(s.traits[0] == "number");
+    REQUIRE(s.content.empty());
+}
+
+TEST_CASE("element signature value and traits", "[signature]")
+{
+    Report r;
+    mdp::ByteBuffer rc;
+    Signature s;
+    
+    SignatureParserHelper::parse("- content is the king",
+                                 ElementTraits::get(), r, rc, s);
+    
+    REQUIRE(r.error.code == Error::OK);
+    REQUIRE(r.warnings.size() == 1);
+    REQUIRE(rc.empty());
+    
+    REQUIRE(s.identifier.empty());
+    REQUIRE(s.value.empty());
+    REQUIRE(s.traits.empty());
+    REQUIRE(s.content == "content is the king");
+}
+
